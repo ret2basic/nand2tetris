674e03c903894f6d06907d54bae95dcdf41ff637
ret2basic <ret2basic@gmail.com>
2022-03-13 00:28:20 -0800

    Project 6

--- projects/06/assemble.sh
+++ projects/06/assemble.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Clean old files
-rm -f Add.hack Max.hack Rect.hack Pong.hack prog.txt project6.zip
+# Clean up old files
+rm -rf submission
 
 # Run the assembler
-python3 hack_assembler.py add/Add.asm
-python3 hack_assembler.py max/Max.asm
-python3 hack_assembler.py rect/Rect.asm
-python3 hack_assembler.py pong/Pong.asm
+python3 assembler/hack_assembler.py add/Add.asm
+python3 assembler/hack_assembler.py max/Max.asm
+python3 assembler/hack_assembler.py rect/Rect.asm
+python3 assembler/hack_assembler.py pong/Pong.asm
 
 # Copy the output files to the current directory
-cp add/Add.hack .
-cp max/Max.hack .
-cp rect/Rect.hack .
-cp pong/Pong.hack .
+mkdir submission
+cp add/Add.hack submission
+cp max/Max.hack submission
+cp rect/Rect.hack submission
+cp pong/Pong.hack submission
 
 # Create a random prog.txt file
-echo "Empty file" > prog.txt
+echo "Empty file" > submission/prog.txt
 
 # Create project6.zip
-zip project6.zip Add.hack Max.hack Rect.hack Pong.hack prog.txt
+zip submission/project6.zip submission/Add.hack submission/Max.hack submission/Rect.hack submission/Pong.hack submission/prog.txt
