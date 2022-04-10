642d63c7b12b2aa36759078274a18563801746cd
ret2basic <ret2basic@gmail.com>
2022-04-10 08:24:48 -0700

    Project 10

--- projects/10/submission.sh
+++ projects/10/submission.sh
@@ -3,7 +3,7 @@
 rm -rf submission
 mkdir submission
 cd jack_compiler
-zip project10.zip jack_analyzer.py jack_tokenizer.py compilation_engine.py lang.txt
+zip project10.zip JackAnalyzer.py jack_tokenizer.py compilation_engine.py lang.txt
 cd ..
 mv jack_compiler/project10.zip submission
 
